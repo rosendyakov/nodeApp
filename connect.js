f3e11f817aa9cfd86420b2f5d8e11b5379a76f27
rosen <r.dyakov1314@gmail.com>
2015-11-24 18:23:17 +0200

    html node test

--- connect.js
+++ connect.js
@@ -1,34 +1,23 @@
-/**
- * Created by rosen on 24/11/2015.
- */
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
-//and our HTTP server
-var http = require('http');
-//setup our port
-var port = process.env.PORT || 1337;
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
 
+            // do some work here with the database.
 
-var url = 'mongodb://rosendyakov:0886540590r@ds054308.mongolab.com:54308/mongodatabase';
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
 
-var MongoClient = mongodb.MongoClient;
-
-MongoClient.connect(url, function (err, db) {
-    response.write('Connection Made \n');
-    if (err) {
-        response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
-        //Error so close connection
-        db.close();
-    } else {
-        //HURRAY!! We are connected. :)
-        response.write('Connection established to' + url +"\n");
-
-        // do some work here with the database.
-
-        //Done Close connection
-        db.close();
-    }
-    response.end('Finished, Connection closed \n');
-});
-
-
+}).listen(port);
