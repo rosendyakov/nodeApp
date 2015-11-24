65ebd7ab069fee48d8870d4205bd19354456dc21
rosen <r.dyakov1314@gmail.com>
2015-11-24 17:43:11 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rosen on 24/11/2015.
+ */
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
