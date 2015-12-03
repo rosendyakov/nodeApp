fc4214ad7df281bfb5d4ce56e79572722285d5f5
rosen <r.dyakov1314@gmail.com>
2015-12-04 01:50:31 +0200

    html node test

---
+++ Insert.js
@@ -0,0 +1,43 @@
+/**
+ * Created by rosen on 04/12/2015.
+ */
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+            var collection = db.collection('users');
+            var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+            collection.insert([user1, user2, user3], function (err, result) {
+                if (err) {
+                    response.write('Insert failed ' + err + "\n");
+                } else {
+                    console.log(result);
+                    response.write('Inserted ' + result.insertedCount +' documents ok. +"\n"');
+                }
+                //Close connection
+                db.close();
+                response.end('Finished, Connection closed \n');
+                //remove any other db.close or response.end statement below this line
+            });
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
