1d135565e67cf668b31b6bc8b7602ad4a5d01c74
rosen <r.dyakov1314@gmail.com>
2015-12-04 01:53:56 +0200

    html node test

---
+++ retrieve.js
@@ -0,0 +1,42 @@
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+            var collection = db.collection('users');
+            var results = collection.find({name: 'modulus user'});
+
+            results.each(function (err, result) {
+                //if the result is null, there are no more results, it’s ok to close everything
+                if (result == null) {
+                    response.end('Completed');
+                    db.close();
+                }
+                if (err) {
+                    response.write(err);
+                } else {
+                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
+                }
+            });
+
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
+/**
+ * Created by rosen on 04/12/2015.
+ */
