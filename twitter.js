330acf99bc79d086bd3b7f66e79d8e495b147648
rosen <r.dyakov1314@gmail.com>
2015-12-04 22:28:28 +0200

    html node test

--- twitter.js
+++ twitter.js
@@ -13,7 +13,8 @@ var client = new Twitter({
 });
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.writeHead(200, { 'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin' : '*' });
 
     client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
         response.end(JSON.stringify(tweets));
