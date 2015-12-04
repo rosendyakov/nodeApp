3a08fc7729b0ad8809c1df090d32040b0b2bccfd
rosen <r.dyakov1314@gmail.com>
2015-12-04 03:12:02 +0200

    html node test

--- retrieve.js
+++ retrieve.js
@@ -23,7 +23,7 @@ http.createServer(function(request, response) {
             //HURRAY!! We are connected. :)
             response.write('Connection established to' + uri +"\n");
             var collection = db.collection('users');
-            var results = collection.find();
+            var results = collection.find({name: 'modulus user'});
             results.each(function (err, result) {
                 //if the result is null, there are no more results, it’s ok to close everything
                 if (result == null) {
