f7af82298c4bafb9c948304561959288e0f07979
rosen <r.dyakov1314@gmail.com>
2015-12-04 02:54:19 +0200

    html node test

--- retrieve.js
+++ retrieve.js
@@ -2,7 +2,7 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Connecting \n');
     // Use connect method to connect to the Server
-    MongoClient.connect(url, function (err, db) {
+    MongoClient.connect(uri, function (err, db) {
         response.write('Connection Made \n');
         if (err) {
             response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
@@ -10,7 +10,7 @@ http.createServer(function(request, response) {
             db.close();
         } else {
             //HURRAY!! We are connected. :)
-            response.write('Connection established to' + url +"\n");
+            response.write('Connection established to' + uri +"\n");
             var collection = db.collection('users');
             var results = collection.find({name: 'modulus user'});
 
